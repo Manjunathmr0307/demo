f5ed6219b734a16d63261ab5aaafcf53ae63c041
manjunathmr0307 <manjunatha0307@gmail.com>
2025-09-09 17:04:45 +0530

    name changed

--- app/01.py
+++ app/01.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.get('/')
 def api():
-    return jsonify({"message": "Hello, World!"})
+    return jsonify({"message": "Hello, Manju!"})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
